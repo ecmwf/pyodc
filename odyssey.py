1bb9c0a7ba94e3137442fc3fd9ecc19b45a868de
Simon Smart <simon.smart@ecmwf.int>
2019-02-13 17:18:32 +0000

    WIP

--- odyssey.py
+++ odyssey.py
@@ -15,21 +15,101 @@
 
 import cffi
 import ctypes.util
+import io
+import os
 
 __version__ = "0.19.0.dev0"
 
 # Locate the C API
 # TODO: Fallback to the python apy
 
-print("CTYPES PATH: ", ctypes.util.find_library("libodbcore.so"))
-
 # TODO: Find this API file properly
 with open('/home/ma/mass/git/odb/odc/odc/src/odc/api/odc_c.h', 'r') as f:
     hdr = f.read()
 
+# print("hdr", hdr)
+
 ffi = cffi.FFI()
 ffi.cdef(hdr)
 
-lib = ffi.dlopen('libodccore.so')
+print("ffi", ffi)
 
-print(lib)
+lib = ffi.dlopen('libodccore.so')
+print("lib", lib)
+lib.odc_error_handling(lib.ODC_ERRORS_CHECKED)
+
+
+# Set up error handling
+
+class ODCException(RuntimeError):
+    pass
+
+
+def __check_errors(helpstring=None):
+
+    if lib.odc_errno != 0:
+        if helpstring is None:
+            helpstring = "make API call"
+
+        helpstring = "Failed to {}: {}".format(helpstring, ffi.string(lib.odc_error_string()))
+        lib.odc_reset_error()
+
+        raise ODCException(helpstring)
+
+
+# Initialise the API, and we are away
+
+
+lib.odc_initialise_api()
+
+
+class Odb:
+
+    __odb = None
+    __tables = None
+
+    def __init__(self, source):
+        if isinstance(source, io.IOBase):
+            self.__odb = lib.odc_
+        else:
+            assert isinstance(source, str)
+            self.__odb = lib.odc_open_for_read(source.encode())
+
+        # Set free function
+        self.__odb = ffi.gc(self.__odb, lib.odc_close)
+
+    @property
+    def tables(self):
+        if self.__tables is None:
+            ntables = lib.odc_num_tables(self.__odb)
+            self.__tables = [
+                Table(ffi.gc(
+                    lib.odc_get_table(self.__odb, n),
+                    lib.odc_free_table
+                ))
+                for n in range(ntables)
+            ]
+
+        return self.__tables
+
+
+class Table:
+
+    def __init__(self, table):
+        self.__table = table
+
+    @property
+    def nrows(self):
+        return lib.odc_table_num_rows(self.__table)
+
+    @property
+    def ncolumns(self):
+        return lib.odc_table_num_columns(self.__table)
+
+    def decode(self):
+        decoded = lib.odc_table_decode_all(self.__table)
+        return None
+
+
+
+
--- tests/test_count.py
+++ tests/test_count.py
@@ -2,7 +2,21 @@
 import pytest
 import odyssey
 
+
 def test_count():
+
     print("We are here")
     print(odyssey.__version__)
+
+    o = odyssey.Odb("/home/ma/mass/testcases/odb_play/BTEM.2.odb")
+
+    tables = o.tables
+
+    print("Num tables: ", len(tables))
+
+    for t in tables:
+        print(t.ncolumns, t.nrows)
+
+    tables[1].decode_all()
+
     assert False
