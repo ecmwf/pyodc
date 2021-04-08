30e85be50df716a97826ac80b7610dbee323f184
Simon Smart <simon.smart@ecmwf.int>
2021-04-08 12:08:21 +0100

    Use check_call in test_setup

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -5,5 +5,4 @@ import subprocess
 
 def test_setup():
     """Performs some tests on the meta-data of a package"""
-    process = subprocess.run([sys.executable, "setup.py", "check"], text=True)
-    assert process.returncode == 0
+    subprocess.check_call([sys.executable, "setup.py", "check"])
