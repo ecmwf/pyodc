a6fc7ca3e8e16e5a077ff063856a9e7560f12d59
Simon Smart <simon.smart@ecmwf.int>
2021-04-08 11:50:54 +0100

    Fix test_setup for various python environments

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,8 +1,9 @@
 import pytest
+import sys
 
 import subprocess
 
 def test_setup():
     """Performs some tests on the meta-data of a package"""
-    process = subprocess.run(["python", "setup.py", "check"], text=True)
+    process = subprocess.run([sys.executable, "setup.py", "check"], text=True)
     assert process.returncode == 0
